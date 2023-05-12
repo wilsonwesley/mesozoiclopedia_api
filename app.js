3bc0fba64c5d817f03e583f9ee5d3043b7de3d68
wilsonwesley <wesley-wilson@outlook.com>
2023-05-12 18:51:18 +0200

    :card_file_box: Database setting up

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const express = require("express");
+// const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+const db = require("./database/dbConnect");
+
+db.then(() => {
+  const connectionStatus = "connected";
+  return connectionStatus;
+});
 
 const app = express();
 app.get("/", function (req, res) {
   res.send("Hello world");
 });
+
+const dinosaurRouter = require("./routes/dinosaur");
+
+// Sert a permettre l'envoie de données JSON dans le body d'une requête
+app.use(express.json());
+
+app.use(cors());
+app.use(cookieParser());
+app.use("/dinosaur", dinosaurRouter);
+
 module.exports = app;
